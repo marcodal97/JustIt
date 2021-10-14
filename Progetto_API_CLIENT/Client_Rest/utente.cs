c069499952cbb9707115db432af3d188cba7a98c
Ire97 <36077841+Ire97@users.noreply.github.com>
2021-10-14 15:50:57 +0200

    Add files via upload

---
+++ Progetto_API_CLIENT/Client_Rest/utente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Progetto_APL_CLIENT.Client_Rest
+{
+    class utente
+    {
+
+        [JsonPropertyName("Username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("Password")]
+        public string Password { get; set; }
+
+
+    }
+}
