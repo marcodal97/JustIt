04e59a7a89538a8e4871ae624903ba226eae430a
Ire97 <36077841+Ire97@users.noreply.github.com>
2021-12-01 18:54:54 +0100

    Update mediatemp.R

--- mediatemp.R
+++ mediatemp.R
@@ -1,23 +1,31 @@
 library(ggplot2)
+
 library(scales)
 
-myArgs <- c(5,4,3,2,1,1,5,4,5,1,2,2,3,3,2)
+myArgs <- commandArgs(trailingOnly = TRUE)
 
-n <- length(myArgs)/3
-result <- split(myArgs, 1:n)
+lista = strtoi(myArgs)
+
+n <- length(lista)/3
+
+result <- split(lista, 1:n)
 
 ordini <- 1:length(result)
 
 valutazioni <- vector()
 
 for(x in result){
+
     media = mean(strtoi(x))
-    valutazioni <- append(valutazioni, media) 
+
+    valutazioni <- append(valutazioni, media)
+
 }
 
 data <- data.frame(ordini, valutazioni)
 
 p = ggplot(data, aes(x=ordini, y=valutazioni)) + geom_line() + scale_x_continuous(breaks=breaks_pretty(length(ordini))) + ylim(1, 5)
+
 file_name = "mediatemp.jpg"  
+
 ggsave(p,file=file_name)
-paste("ok")
